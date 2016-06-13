43c67c0c6075c8676f1ec251e792528e37c2eece
Matthew Scott <matt@11craft.com>
2016-06-12 23:37:01 -0500

    Script to watch for file changes and show diff of reader output

---
+++ watchfiledump.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+watchmedo shell-command -c "clear; mv $1.dump $1.dump.orig; python -m rv.tools.filedump $1.sunvox 2>&1 | tee $1.dump; echo; diff -Naur $1.dump.orig $1.dump" -p '*.sunvox;*.py' -R -W .
