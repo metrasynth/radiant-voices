0f0d544e41221f3d82071d69078e21d5f1de1212
Matthew Scott <matt@11craft.com>
2016-07-09 14:16:17 -0500

    Organization

--- rv/structure/modules/analog_generator.py
+++ rv/structure/modules/analog_generator.py
@@ -4,6 +4,32 @@ from rv.structure.controller import Controller, Range
 from rv.structure.module import GenericModule
 
 
+class AnalogGeneratorModule(GenericModule):
+
+    # TODO: CHNK, CHNM, CHDT, CHFF, CHFR
+
+    class controllers(object):
+        volume = Controller(0x01, Range(0, 256))
+        waveform = Controller(0x02, Waveform)
+        panning = Controller(0x03, Range(-128, 128))
+        attack = Controller(0x04, Range(0, 256))
+        release = Controller(0x05, Range(0, 256))
+        sustain = Controller(0x06, bool)
+        exponential_envelope = Controller(0x07, bool)
+        duty_cycle = Controller(0x08, Range(0, 1024))
+        freq2 = Controller(0x09, Range(0, 2000))
+        filter = Controller(0x0a, Filter)
+        f_freq_hz = Controller(0x0b, Range(0, 14000))
+        f_resonance = Controller(0x0c, Range(0, 1530))
+        f_exponential_freq = Controller(0x0d, bool)
+        f_attack = Controller(0x0e, Range(0, 256))
+        f_release = Controller(0x0f, Range(0, 256))
+        f_envelope = Controller(0x10, FilterEnvelope)
+        polyphony_ch = Controller(0x11, Range(1, 32))
+        mode = Controller(0x12, Mode)
+        noise = Controller(0x13, Range(0, 256))
+
+
 class Waveform(IntEnum):
 
     TRIANGLE = 0
@@ -42,29 +68,3 @@ class Mode(IntEnum):
     HQ_MONO = 1
     LQ = 2
     LQ_MONO =3
-
-
-class AnalogGeneratorModule(GenericModule):
-
-    # TODO: CHNK, CHNM, CHDT, CHFF, CHFR
-
-    class controllers(object):
-        volume = Controller(0x01, Range(0, 256))
-        waveform = Controller(0x02, Waveform)
-        panning = Controller(0x03, Range(-128, 128))
-        attack = Controller(0x04, Range(0, 256))
-        release = Controller(0x05, Range(0, 256))
-        sustain = Controller(0x06, bool)
-        exponential_envelope = Controller(0x07, bool)
-        duty_cycle = Controller(0x08, Range(0, 1024))
-        freq2 = Controller(0x09, Range(0, 2000))
-        filter = Controller(0x0a, Filter)
-        f_freq_hz = Controller(0x0b, Range(0, 14000))
-        f_resonance = Controller(0x0c, Range(0, 1530))
-        f_exponential_freq = Controller(0x0d, bool)
-        f_attack = Controller(0x0e, Range(0, 256))
-        f_release = Controller(0x0f, Range(0, 256))
-        f_envelope = Controller(0x10, FilterEnvelope)
-        polyphony_ch = Controller(0x11, Range(1, 32))
-        mode = Controller(0x12, Mode)
-        noise = Controller(0x13, Range(0, 256))
--- rv/structure/modules/input.py
+++ rv/structure/modules/input.py
@@ -4,14 +4,14 @@ from rv.structure.controller import Controller, Range
 from rv.structure.module import GenericModule
 
 
-class Channels(IntEnum):
-
-    MONO = 0
-    STEREO = 1
-
-
 class InputModule(GenericModule):
 
     class controllers(object):
         volume = Controller(0x01, Range(0, 1024))
         channels = Controller(0x02, Channels)
+
+
+class Channels(IntEnum):
+
+    MONO = 0
+    STEREO = 1
