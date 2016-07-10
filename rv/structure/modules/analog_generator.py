dfd9e3fd88e3f6a1a8efc4cf492402ea728d7cb5
Matthew Scott <matt@11craft.com>
2016-07-10 16:31:22 -0500

    Correct ordering

--- rv/structure/modules/analog_generator.py
+++ rv/structure/modules/analog_generator.py
@@ -12,32 +12,6 @@ class Mode(IntEnum):
     LQ_MONO = 3
 
 
-class AnalogGeneratorModule(GenericModule):
-
-    # TODO: CHNK, CHNM, CHDT, CHFF, CHFR
-
-    class controller_types:
-        volume = Controller(0x01, (0, 256))
-        waveform = Controller(0x02, Waveform)
-        panning = Controller(0x03, (-128, 128))
-        attack = Controller(0x04, (0, 256))
-        release = Controller(0x05, (0, 256))
-        sustain = Controller(0x06, bool)
-        exponential_envelope = Controller(0x07, bool)
-        duty_cycle = Controller(0x08, (0, 1024))
-        freq2 = Controller(0x09, (0, 2000))
-        filter = Controller(0x0a, Filter)
-        f_freq_hz = Controller(0x0b, (0, 14000))
-        f_resonance = Controller(0x0c, (0, 1530))
-        f_exponential_freq = Controller(0x0d, bool)
-        f_attack = Controller(0x0e, (0, 256))
-        f_release = Controller(0x0f, (0, 256))
-        f_envelope = Controller(0x10, FilterEnvelope)
-        polyphony_ch = Controller(0x11, (1, 32))
-        mode = Controller(0x12, Mode)
-        noise = Controller(0x13, (0, 256))
-
-
 class Waveform(IntEnum):
 
     TRIANGLE = 0
@@ -70,3 +44,27 @@ class FilterEnvelope(IntEnum):
     SUSTAIN_ON = 2
 
 
+class AnalogGeneratorModule(GenericModule):
+
+    # TODO: CHNK, CHNM, CHDT, CHFF, CHFR
+
+    class controller_types:
+        volume = Controller(0x01, (0, 256))
+        waveform = Controller(0x02, Waveform)
+        panning = Controller(0x03, (-128, 128))
+        attack = Controller(0x04, (0, 256))
+        release = Controller(0x05, (0, 256))
+        sustain = Controller(0x06, bool)
+        exponential_envelope = Controller(0x07, bool)
+        duty_cycle = Controller(0x08, (0, 1024))
+        freq2 = Controller(0x09, (0, 2000))
+        filter = Controller(0x0a, Filter)
+        f_freq_hz = Controller(0x0b, (0, 14000))
+        f_resonance = Controller(0x0c, (0, 1530))
+        f_exponential_freq = Controller(0x0d, bool)
+        f_attack = Controller(0x0e, (0, 256))
+        f_release = Controller(0x0f, (0, 256))
+        f_envelope = Controller(0x10, FilterEnvelope)
+        polyphony_ch = Controller(0x11, (1, 32))
+        mode = Controller(0x12, Mode)
+        noise = Controller(0x13, (0, 256))
