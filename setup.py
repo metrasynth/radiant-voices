10de84a6640cf04daa34aee0c83686c0e3dee8e1
Matthew Scott <matt@11craft.com>
2016-08-15 23:23:20 -0500

    Move SunVox DLL wrapper to metrasynth/sunvox-dll-python

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 """
-Radiant Voice wraps the SunVox DLL.
+Radiant Voices aims to give you a full set of tools to
+create, manipulate, and write SunVox song and synth files.
 """
 from setuptools import find_packages, setup
 
@@ -13,7 +14,7 @@ setup(
     license='BSD',
     author='Matthew Scott',
     author_email='matt@11craft.com',
-    description='Wrapper for SunVox DLL and reader/writer for .sunvox and .sunsynth files',
+    description='Create, read, modify, and write SunVox files',
     long_description=__doc__,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
@@ -25,9 +26,9 @@ setup(
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 1 - Planning',
-        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
+        # 'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
