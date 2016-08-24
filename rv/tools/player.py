938192b587167c9f4989ab9679758bed5684fa35
Matthew Scott <matt@11craft.com>
2016-08-24 17:06:13 -0500

    Add sunsynth support to player, and a --send-event option

--- rv/tools/player.py
+++ rv/tools/player.py
@@ -8,13 +8,16 @@ then save it back to an in-memory buffer,
 then play that resulting project using SunVox DLL.
 """
 
-import argparse
 import logging
 log = logging.getLogger(__name__)
 
+import argparse
+from os import unlink
+from tempfile import mkstemp
+
 import sunvox
 
-from rv import read_sunvox_file
+from rv import ENCODING, Project, Synth, read_sunvox_file
 
 
 parser = argparse.ArgumentParser(description='Radiant Voices Player')
@@ -23,6 +26,10 @@ parser.add_argument('filename', metavar='FILE', type=str, nargs=1,
 parser.add_argument('--skip-rv', dest='skip_rv', action='store_const',
                     const=True, default=False,
                     help='Skip Radiant Voices and play directly using SunVox')
+parser.add_argument('--send-event', dest='send_event', action='store',
+                    metavar='TRACK,NOTE,VEL,MODULE,CTL,EFFECT,VAL',
+                    default=None,
+                    help='Instead of playing, send a command')
 
 
 def main():
@@ -31,17 +38,51 @@ def main():
     filename = args.filename[0]
     if args.skip_rv:
         log.debug('Skipping Radiant Voices')
-        project = filename
+        obj = filename
     else:
         log.debug('Loading into Radiant Voices')
-        project = read_sunvox_file(filename)
+        obj = read_sunvox_file(filename)
     sunvox.init(None, 44100, 2, 0)
-    with sunvox.Slot(project) as slot:
-        log.info('{} modules loaded'.format(slot.get_number_of_modules()))
+    slot = sunvox.Slot()
+    if isinstance(obj, Project):
+        slot.load(obj)
+    elif isinstance(obj, Synth):
+        _, filename = mkstemp()
+        filename = filename.encode('utf8')
+        try:
+            with open(filename, 'wb') as f:
+                obj.write_to(f)
+            slot.load_module(filename, 0, 0, 0)
+            slot.connect_module(1, 0)
+            slot.volume(255)
+        finally:
+            unlink(filename)
+    elif isinstance(obj, str):
+        filename = obj.encode('utf8')
+        if obj.endswith('.sunvox'):
+            slot.load_filename(filename)
+        elif obj.endswith('.sunsynth'):
+            slot.load_module(filename, 0, 0, 0)
+            slot.connect_module(1, 0)
+            slot.volume(255)
+        else:
+            log.warn('Unknown file extension')
+    module_count = slot.get_number_of_modules()
+    log.info('{} modules loaded'.format(module_count))
+    for i in range(module_count):
+        log.debug('module {}: {}'.format(
+            i, slot.get_module_name(i).decode(ENCODING)))
+    if args.send_event is not None:
+        event_args = [int(x) for x in args.send_event.split(',')]
+        log.debug('event_args {!r}'.format(event_args))
+        track, note, vel, module, ctl, effect, val = event_args
+        ctl_effect = ctl << 16 + effect
+        slot.send_event(track, note, vel, module, ctl_effect, val)
+    else:
         slot.play_from_beginning()
-        print('Press Enter to stop playback')
-        input()
-        slot.stop()
+    print('Press Enter to stop playback')
+    input()
+    slot.stop()
     sunvox.deinit()
 
 
