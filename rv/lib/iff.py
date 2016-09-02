331bdb004dce0c31c510d4820e2a8156330047f3
Matthew Scott <matt@11craft.com>
2016-09-02 09:39:34 -0500

    Split IFF dump tool into functions that can be called from other modules

--- rv/lib/iff.py
+++ rv/lib/iff.py
@@ -25,24 +25,32 @@ def chunks(f):
             break
 
 
-if __name__ == '__main__':
-    import argparse
+def dump_file(f):
     from hexdump import hexdump
     from rv import ENCODING
+    for name, data in chunks(f):
+        print(name.decode(ENCODING), end='  ')
+        i = None
+        for i, line in enumerate(hexdump(data, 'generator')):
+            if i > 0:
+                print('      ' + line)
+            else:
+                print(line)
+        if i is None:
+            print()
+        print()
+
+
+def dump_tool():
+    import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', metavar='FILE', type=str, nargs=1,
                         help='File to load and dump')
     args = parser.parse_args()
     filename = args.filename[0]
     with open(filename, 'rb') as f:
-        for name, data in chunks(f):
-            print(name.decode(ENCODING), end='  ')
-            i = None
-            for i, line in enumerate(hexdump(data, 'generator')):
-                if i > 0:
-                    print('      ' + line)
-                else:
-                    print(line)
-            if i is None:
-                print()
-            print()
+        dump_file(f)
+
+
+if __name__ == '__main__':
+    dump_tool()
