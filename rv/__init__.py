1faa3dce35dba5d722286a8e829ec1da262edc9d
Matthew Scott <matt@11craft.com>
2016-09-02 20:08:00 -0500

    Split NOTECMD into NOTE and NOTECMD

--- rv/__init__.py
+++ rv/__init__.py
@@ -2,7 +2,7 @@
 ENCODING = 'cp1251'
 
 import rv.modules
-from rv.note import NOTECMD, Note
+from rv.note import Note, NOTE, NOTECMD
 from rv.pattern import Pattern, PatternClone
 from rv.project import Project
 from rv.readers.reader import read_sunvox_file
--- rv/note.py
+++ rv/note.py
@@ -6,7 +6,9 @@ from attr import attr, attributes
 from rv.lib.validators import in_range
 
 
-class NOTECMD(IntEnum):
+class NOTE(IntEnum):
+    """All notes available for patterns or mapping."""
+
     (C0, d0, D0, e0, E0, F0, G0, a0, A0, b0, B0,
      C1, d1, D1, e1, E1, F1, G1, a1, A1, b1, B1,
      C2, d2, D2, e2, E2, F2, G2, a2, A2, b2, B2,
@@ -17,6 +19,22 @@ class NOTECMD(IntEnum):
      C7, d7, D7, e7, E7, F7, G7, a7, A7, b7, B7,
      C8, d8, D8, e8, E8, F8, G8, a8, A8, b8, B8,
      C9, d9, D9, e9, E9, F9, G9, a9, A9, b9, B9) = range(1, 111)
+
+
+class NOTECMD(IntEnum):
+    """All notes and commands available for patterns."""
+
+    (C0, d0, D0, e0, E0, F0, G0, a0, A0, b0, B0,
+     C1, d1, D1, e1, E1, F1, G1, a1, A1, b1, B1,
+     C2, d2, D2, e2, E2, F2, G2, a2, A2, b2, B2,
+     C3, d3, D3, e3, E3, F3, G3, a3, A3, b3, B3,
+     C4, d4, D4, e4, E4, F4, G4, a4, A4, b4, B4,
+     C5, d5, D5, e5, E5, F5, G5, a5, A5, b5, B5,
+     C6, d6, D6, e6, E6, F6, G6, a6, A6, b6, B6,
+     C7, d7, D7, e7, E7, F7, G7, a7, A7, b7, B7,
+     C8, d8, D8, e8, E8, F8, G8, a8, A8, b8, B8,
+     C9, d9, D9, e9, E9, F9, G9, a9, A9, b9, B9) = range(1, 111)
+
     EMPTY = 0
     NOTE_OFF = 128
     ALL_NOTES_OFF = 129  # notes of all synths off
@@ -27,6 +45,7 @@ class NOTECMD(IntEnum):
 
 @attributes(slots=True)
 class Note(object):
+    """A single note, for use within a :py:class:`Pattern`."""
 
     note = attr(convert=NOTECMD, default=NOTECMD.EMPTY)
     vel = attr(convert=int, validator=in_range(0, 129), default=0)
