058c7327b3a160b7738217097257bea928b92099
Matthew Scott <matt@11craft.com>
2016-09-02 20:21:08 -0500

    Auto-generate documentation for rv.modules classes

--- rv/modules/meta.py
+++ rv/modules/meta.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from textwrap import dedent
 
 from rv.controller import Controller
 from rv.modules import MODULE_CLASSES
@@ -20,3 +21,6 @@ class ModuleMeta(type):
             cls.controllers[k] = v
         if class_dict.get('mtype') is not None:
             MODULE_CLASSES[class_dict['mtype']] = cls
+        cls.__doc__ = dedent("""\
+            "{mtype}" SunVox {mgroup} Module
+        """).format(**class_dict)
