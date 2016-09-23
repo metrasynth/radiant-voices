6ea17c51b147de0048013945c158e52f00aaeabe
Matthew Scott <matt@11craft.com>
2016-09-22 21:26:22 -0500

    MultiSynth: add options, note/velocity curve, velocity/velocity curve

--- rv/modules/multisynth.py
+++ rv/modules/multisynth.py
@@ -1,13 +1,27 @@
+from rv.chunks import CurveChunk
 from rv.controller import Controller
 from rv.modules import Module
+from rv.option import Option
 
 
 class MultiSynth(Module):
 
     name = mtype = 'MultiSynth'
     mgroup = 'Misc'
+    chnk = 0x10
+    options_chnm = 1
 
-    # TODO: CHNK, CHNM, CHDT, CHFF, CHFR
+    class NoteVelocityCurve(CurveChunk):
+        chnm = 0
+        length = 128
+        type = 'B'
+        default = [0xff] * length
+
+    class VelocityVelocityCurve(CurveChunk):
+        chnm = 2
+        length = 257
+        type = 'B'
+        default = list(range(256)) + [0xff]
 
     transpose = Controller((-128, 128), 0)
     random_pitch = Controller((0, 4096), 0)
@@ -17,23 +31,37 @@ class MultiSynth(Module):
     random_velocity = Controller((0, 32768), 0)
     phase = Controller((0, 32768), 0)
 
+    use_static_note_C5 = Option(False)
+    ignore_notes_with_zero_velocity = Option(False)
+    vv_curve_active = Option(False)
 
-"""
-CHNK: 00000010
-CHFF: 00000000
-CHFR: 00000000
+    def __init__(self, **kwargs):
+        nv_values = kwargs.pop('nv_values', None)
+        vv_values = kwargs.pop('vv_values', None)
+        super(MultiSynth, self).__init__(**kwargs)
+        self.nv_curve = self.NoteVelocityCurve()
+        self.vv_curve = self.VelocityVelocityCurve()
+        if nv_values is not None:
+            self.nv_curve.values = nv_values
+        if vv_values is not None:
+            self.vv_curve.values = vv_values
 
-CHNM: 0
-CHDT: <128 bytes representing velocity curve (x = note in, y = velocity out)
+    def specialized_iff_chunks(self):
+        for chunk in self.nv_curve.chunks():
+            yield chunk
+        for chunk in self.vv_curve.chunks():
+            yield chunk
 
-CHNM: 1
-CHDT: options, 64 bytes:
-        0: use static note C5
-        1: ignore notes with zero velocity
-        2: velocity/velocity curve active
-        3:
-        4-63: zero padding
+    def load_chunk(self, chunk):
+        if chunk.chnm == self.options_chnm:
+            self.load_options(chunk)
+        elif chunk.chnm == 0:
+            self.load_nv_curve(chunk)
+        elif chunk.chnm == 2:
+            self.load_vv_curve(chunk)
 
-CHNM: 2
-CHDT: <257 bytes representing velocity curve (x = velocity in, y = velocity out)
-"""
+    def load_nv_curve(self, chunk):
+        self.nv_curve.bytes = chunk.chdt
+
+    def load_vv_curve(self, chunk):
+        self.vv_curve.bytes = chunk.chdt
