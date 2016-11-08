9198901d9718549698460fab1eb531c11a22f3a2
Matthew Scott <matt@11craft.com>
2016-11-07 21:29:02 -0600

    Enable PlantUML rendering

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'plantweb.directive',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
