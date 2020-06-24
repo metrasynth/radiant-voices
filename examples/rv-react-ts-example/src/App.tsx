3b3ba75544abd43bfd5e19e13bf96c34cace9dd0
Matthew Scott <matt@11craft.com>
2020-06-23 22:46:12 -0500

    Barebones example of react+typescript+sunvox

---
+++ examples/rv-react-ts-example/src/App.tsx
@@ -0,0 +1,51 @@
+import React, { useCallback } from "react"
+import { useDropzone } from "react-dropzone"
+import * as sunvox from "./sunvox-lib-loader"
+
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+  const onDrop = useCallback((acceptedFiles) => {
+    if (acceptedFiles.length > 0 && acceptedFiles[0].type === "audio/sunvox") {
+      const acceptedFile = acceptedFiles[0]
+      console.log(acceptedFile)
+      const reader = new FileReader()
+      reader.onload = () => {
+        const buf = reader.result as ArrayBuffer
+        sunvox.sv_open_slot(0)
+        const arr = new Uint8Array(buf)
+        sunvox.sv_load_from_memory(0, arr)
+        sunvox.sv_play_from_beginning(0)
+      }
+      reader.readAsArrayBuffer(acceptedFile)
+    }
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+  return (
+    <div className="App">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        {isDragActive ? (
+          <p>Drop the files here</p>
+        ) : (
+          <p>Drag 'n' drop files here, or click to select files</p>
+        )}
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
