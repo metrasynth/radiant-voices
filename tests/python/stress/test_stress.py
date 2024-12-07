45c069bbdf2119e0d87b5fba0cd02bc8d928ed41
Matthew Scott <matt@11craft.com>
2024-12-06 23:20:31 -0600

    stress tests to load and save all official sunvox examples

---
+++ tests/python/stress/test_stress.py
@@ -0,0 +1,31 @@
+"""If `sunvox/` is adjacent to this repo, load and save (to memory) all SunVox files"""
+from io import BytesIO
+from pathlib import Path
+
+import pytest
+from rv.api import read_sunvox_file
+
+sunvox_dir = Path(__file__).parents[4] / "sunvox"
+filenames = []
+if sunvox_dir.is_dir():
+    sunvox_dir = sunvox_dir.absolute().resolve()
+    path: Path
+    for path in sunvox_dir.glob("**/*.sunvox"):
+        filenames.append(path.relative_to(sunvox_dir))
+    for path in sunvox_dir.glob("**/*.sunsynth"):
+        filenames.append(path.relative_to(sunvox_dir))
+
+
+def make_test_id(filename):
+    return str(filename)
+
+
+@pytest.mark.parametrize("filename", filenames, ids=make_test_id)
+def test_stress(filename):
+    path = sunvox_dir / filename
+    with path.open("rb") as f:
+        obj = read_sunvox_file(f)
+    f = BytesIO()
+    obj.write_to(f)
+    f.seek(0)
+    read_sunvox_file(f)
